4f2ccb31ecad0d2f03436fe5e24877651eb7b251
DanielJaque <91282996+DanielJaque@users.noreply.github.com>
2021-10-13 16:57:41 -0300

    Create actividad1.c

---
+++ programacion-7/actividad1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int i, par;
+printf("los numeros pares entre 0 y 100 son:\n");
+  for(i=1; i<51 ; i++)
+   {
+    par= 2 * i;
+    printf("%d\n",par);
+   }
+    return 0;
+}
